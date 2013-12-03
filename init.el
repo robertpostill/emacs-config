eb89f4e843834eee130dcec264dfc90d1f958627
Robert Postill <rob.postill@myob.com>
2013-12-03 22:54:36 +1100

    Attempt #101010101010 to store my emacs conifg.

---
+++ init.el
@@ -0,0 +1,54 @@
+(setq inhibit-startup-screen +1)
+
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
+
+;; ido mode - what would I do without it?
+(setq ido-enable-flex-matching t)
+(ido-mode +1)
+; (ido-yes-or-no-mode +1)
+
+;; LISP hacking
+;; Setup shamelessly ripped from 
+;; http://zeekat.nl/articles/making-emacs-work-for-me.html
+(defun my/->string (str)
+  (cond
+   ((stringp str) str)
+   ((symbolp str) (symbol-name str))))
+
+(defun my/->mode-hook (name)
+  "Turn mode name into hook symbol"
+  (intern (replace-regexp-in-string "\\(-mode\\)?\\(-hook\\)?$"
+                                    "-mode-hook"
+                                    (my/->string name))))
+(defun my/->mode (name)
+  "Turn mode name into mode symbol"
+  (intern (replace-regexp-in-string "\\(-mode\\)?$"
+                                    "-mode"
+                                    (my/->string name))))
+
+(defun my/turn-on (&rest mode-list)
+  "Turn on the given (minor) modes."
+  (dolist (m mode-list)
+    (funcall (my/->mode m) +1)))
+
+(setq my/lisps
+      '(emacs-lisp lisp clojure))
+
+(defun my/general-lisp-hooks ()
+  (my/turn-on 'paredit
+              'rainbow-delimiters
+              'highlight-parentheses))
+
+(dolist (mode (mapcar 'my/->mode-hook my/lisps))
+  (add-hook mode
+            'my/general-lisp-hooks))
+
+;; Ruby 
+(dolist (exp '("Rakefile\\'" "\\.rake\\'"))
+  (add-to-list 'auto-mode-alist
+               (cons exp 'ruby-mode)))
+
+(load-theme 'solarized-dark t)
+
+(server-start)

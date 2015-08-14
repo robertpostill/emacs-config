b51834c15c437297e6ad0c7086133d22d2928b1e
Robert Postill <rob.postill@myob.com>
2015-08-14 23:23:32 +1000

    Use chai JS and some added clojurescript love.

--- init.el
+++ init.el
@@ -172,6 +172,7 @@
  '(custom-safe-themes
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(magit-commit-arguments nil)
  '(utop-command "opam config exec \"utop -emacs\""))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.

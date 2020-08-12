70a86e30f1d9f92b4d7178a195fc8b1e87da6f7a
Robert Postill <robert@grinning-cat.com>
2020-08-13 00:28:34 +1000

    bootstrap straight.el

--- init.el
+++ init.el
@@ -7,3 +7,17 @@
 
 (setq inhibit-startup-screen +1) 
 (global-visual-line-mode 1) ; word wraping
+
+;; setup straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))

c85eb3fb3c1f531e74ae64467287683085611eae
Robert Postill <robert@grinning-cat.com>
2020-08-13 11:41:03 +1000

    setup ivy

--- init.el
+++ init.el
@@ -91,6 +91,60 @@
 	     :config
 	     (keychain-refresh-environment))
 
+;; ivy and firends
+(use-package ivy
+  :straight t
+  :init
+  :config
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t)
+  :bind
+  (("\C-s" . 'swiper)
+   ("C-c C-r" . 'ivy-resume)
+   ("<f6>" .'ivy-resume)
+   ("M-x" . 'counsel-M-x)
+   ("C-c g" . 'counsel-git)
+   ("C-c j" . 'counsel-git-grep)
+   ("C-c k" . 'counsel-ag)
+   ("C-x l" .'counsel-locate)))
+
+(use-package swiper
+  :straight t
+  :after ivy
+  :bind ("C-M-s" . swiper)
+  :bind (:map swiper-map
+              ("M-y" . yank)
+              ("M-%" . swiper-query-replace)
+              ("C-." . swiper-avy)
+              ("M-c" . swiper-mc))
+  :bind (:map isearch-mode-map
+              ("C-o" . swiper-from-isearch)))
+
+(use-package counsel
+  :straight t
+  :after ivy
+  :demand t
+  :diminish
+  :custom (counsel-find-file-ignore-regexp
+           (concat "\\(\\`\\.[^.]\\|"
+                   (regexp-opt completion-ignored-extensions)
+                   "\\'\\)"))
+  :bind (("C-*"     . counsel-org-agenda-headlines)
+         ("C-x C-f" . counsel-find-file)
+         ("C-c e l" . counsel-find-library)
+         ("C-c e q" . counsel-set-variable)
+         ("C-h f"   . counsel-describe-function)
+         ("C-x r b" . counsel-bookmark)
+         ("M-x"     . counsel-M-x)
+         ;; ("M-y"     . counsel-yank-pop)
+
+         ("M-s f" . counsel-file-jump)
+         ;; ("M-s g" . counsel-rg)
+         ("M-s j" . counsel-dired-jump))
+  :commands counsel-minibuffer-history
+  :init
+  (bind-key "C-r" #'counsel-minibuffer-history minibuffer-local-map))
+
 ;; terraform developemnt
 (use-package terraform-mode
   :straight t)

4128d752bd0141aae5dcf316693f41148c1fe469
Robert Postill <robert@grinning-cat.com>
2023-04-30 17:15:35 +1000

    better JS

--- init.el
+++ init.el
@@ -195,7 +195,23 @@
   :init
   (bind-key "C-r" #'counsel-minibuffer-history minibuffer-local-map))
 
-(use-package sqlite3
+(use-package emacsql-sqlite3
+  :straight t)
+
+(use-package tree-sitter
+  :straight t
+  :config
+  ;; activate tree-sitter on any buffer containing code for which it has a parser available
+  (global-tree-sitter-mode)
+  ;; you can easily see the difference tree-sitter-hl-mode makes for python, ts or tsx
+  ;; by switching on and off
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+
+(use-package tree-sitter-langs
+  :straight t
+  :after tree-sitter)
+
+(use-package eglot
   :straight t)
 
 ;; Org usage
@@ -265,15 +281,22 @@
 
 ;; Javascript
 (setq js-indent-level 2)
+(use-package js-mode
+  :hook ((js-mode . 'js2-minor-mode)))
+(use-package js2-mode
+  :straight t)
 
 ;; Typescript
 (use-package typescript-mode
+  :after tree-sitter
   :straight t
   :mode "\\.ts\\'"
   :config (setq-default typescript-indent-level 2))
 
 (use-package prettier-js
-  :straight t)
+  :straight t
+  ;:hook ((js-mode . 'prettier-js-mode))
+  )
 
 (use-package jsdoc
   :straight (:host github :repo "isamert/jsdoc.el"))
@@ -320,6 +343,16 @@
               (setup-tide-mode))))
 ;; enable typescript-tslint checker
 (flycheck-add-mode 'typescript-tslint 'web-mode)
+
+(use-package tsi
+  :after tree-sitter
+  :straight (tsi :type git :host github :repo "orzechowskid/tsi.el")
+  :commands (tsi-typescript-mode tsi-json-mode tsi-css-mode)
+  :init
+  (add-hook 'typescript-mode-hook (lambda () (tsi-typescript-mode 1)))
+  (add-hook 'json-mode-hook (lambda () (tsi-json-mode 1)))
+  (add-hook 'css-mode-hook (lambda () (tsi-css-mode 1)))
+  (add-hook 'scss-mode-hook (lambda () (tsi-scss-mode 1))))
 
 ;; ReasonML
 (use-package reason-mode

e65bd8e21b14876c9e6d408ee051327156a6fd7e
Robert Postill <robert@grinning-cat.com>
2025-02-02 18:01:45 +1100

    use treeview
    and of course show all the files

--- init.el
+++ init.el
@@ -86,6 +86,12 @@
   :bind
   ("C-\"" . 'ace-window))
 
+(use-package dir-treeview
+  :straight t
+  :config
+  (load-theme 'dir-treeview-pleasant t)
+  :bind ("<f9>" . 'dir-treeview))
+
 ;; subtrees, visualised
 (use-package dired-subtree
   :straight t
@@ -445,6 +451,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" "7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" default))
+ '(dir-treeview-show-hidden-files t)
  '(ispell-program-name "aspell"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.

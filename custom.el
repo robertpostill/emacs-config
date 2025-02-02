878bac7d7f0167d89978bb43b4e753d6810126a4
Robert Postill <robert@grinning-cat.com>
2025-02-02 18:06:52 +1100

    moved Emacs custom variables to their own file

---
+++ custom.el
@@ -0,0 +1,23 @@
+;;; init.el --- Initialisation file for Emacs  -*- lexical-binding: t; -*-
+;;; Commentary:
+;; Emacs Startup File --- initialisation for Emacs
+;; Robert Postill's Emacs config
+;;
+
+;;; Code:
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" "7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" default))
+ '(dir-treeview-show-hidden-files t)
+ '(ispell-program-name "aspell"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+;;; custom.el ends here
--- init.el
+++ init.el
@@ -20,6 +20,8 @@
 (put 'upcase-region 'disabled nil) ; yes I really want to upcase regions.
 (setq-default indent-tabs-mode nil) ; don't use tabs unless your major mode demands it
 (fset 'yes-or-no-p 'y-or-n-p) ;; Use y/n instead of yes/no
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
 ;; suppress the error message when using dired
 (when (string= system-type "darwin")
   (setq dired-use-ls-dired nil))
@@ -81,7 +83,7 @@
   (midnight-delay-set 'midnight-delay 0))
 
 ;; window movement made nice
-(use-package ace-window
+......(use-package ace-window
   :straight t
   :bind
   ("C-\"" . 'ace-window))
@@ -445,18 +447,3 @@
 
 (provide 'init)
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" "7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" default))
- '(dir-treeview-show-hidden-files t)
- '(ispell-program-name "aspell"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
